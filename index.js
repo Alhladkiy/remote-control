bdc2b193961a89b5b49fef8cc801373ade98def1
Alhladkiy <Alhladkiy@gmail.com>
2022-06-25 20:19:19 +0300

    feat: add webSocket server

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ import {httpServer} from './src/http_server/index.js';
 import robot from 'robotjs';
 import { WebSocketServer } from 'ws';
 
+
 const HTTP_PORT = 3000;
+const WS_PORT = 8080;
+
+const wss = new WebSocketServer({ port: WS_PORT });
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        console.log('hello i am websocket')
+    })
+    wss.on('close', () => {
+        console.log('exit server');
+    });
+})
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+// console.log(robot.getMousePos())
