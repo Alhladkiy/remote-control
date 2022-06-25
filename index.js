c959c48d8ea6f2bf536c0a2c91e47dc4e6c25501
Alhladkiy <Alhladkiy@gmail.com>
2022-06-25 23:34:43 +0300

    feat: add mouse_up and mouse_right commands

--- index.js
+++ index.js
@@ -1,23 +1,70 @@
 import Jimp from 'jimp';
 import {httpServer} from './src/http_server/index.js';
 import robot from 'robotjs';
-import { WebSocketServer } from 'ws';
+import { createWebSocketStream, WebSocketServer } from 'ws';
+import pkg from 'robotjs';
 
 
+const { getMousePos } = pkg;
 const HTTP_PORT = 3000;
 const WS_PORT = 8080;
-
-const wss = new WebSocketServer({ port: WS_PORT });
-wss.on('connection', (ws) => {
-    ws.on('message', (message) => {
-        console.log('hello i am websocket')
-    })
-    wss.on('close', () => {
-        console.log('exit server');
-    });
-})
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
-// console.log(robot.getMousePos())
+const wsServer = new WebSocketServer({ port: WS_PORT });
+// wss.on('connection', ws => {
+//     ws.on('message', data => {
+//         const {x, y } = robot.getMousePos();
+//         ws.send(`mouse position ${x, y}`);
+//     })
+// })
+// wss.on('close', () => {
+//     console.log('exit server');
+// });
+
+
+wsServer.on('connection', (ws) => {
+    const webSocketStream = createWebSocketStream(ws, { encoding: 'utf8', decodeStrings: false })
+  
+    let data = '';
+  
+    webSocketStream.on('readable', () => {
+        data = webSocketStream.read();
+        webSocketStream.emit('end');
+    });
+  
+    webSocketStream.on('end', () => {
+        if (!data) { 
+            console.log('Invaled data') 
+        }
+        console.log(data)
+        let { x, y } = robot.getMousePos();
+  
+        const [command, coordinatesX, coordinatesY] = data.split(' ');
+        const width =+ coordinatesX;
+        const height =+ coordinatesY;
+        const prevValue = robot.getMousePos();
+  
+        if (command === 'mouse_up') {
+            y -= width;
+            console.log('prev y:', prevValue.y, 'cur y:', y)
+            robot.moveMouse(x, y);
+        }
+    
+        if (command === 'mouse_right') {
+            x += width;
+            console.log('prev x:', prevValue.x, 'cur x:', x)
+            robot.moveMouse(x, y);
+        }
+
+        ws.send(command);
+    });
+  
+        ws.on('close', (err) => {
+            if (err) {
+                console.log (`Error ${err}`)
+            }
+            console.log('server finished');
+        });
+    });
